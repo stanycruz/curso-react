54de20bd3306633c234e76f4f7eeaf9eaad33cff
Stanislaw Cruz <stanycruz@outlook.com>
2021-07-27 10:26:14 -0300

    Componente com Propriedade.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do Aluno"
+            aluno="Stany Cruz" nota={9.3} />
     </div>,
     document.getElementById('root')
 )
