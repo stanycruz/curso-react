08ce2f4fb1b6cf54f49485a689c1e152a4b9bb63
Stanislaw Cruz <stanycruz@outlook.com>
2021-07-28 17:00:19 -0300

    Repetição

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 
+import ListaAlunos from './components/repeticao/ListaAlunos'
 import Familia from './components/basicos/Familia'
 import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Card from './components/layout/Card'
@@ -14,6 +15,10 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
             <Card titulo="#05 - Componentes Filhos" color="#00C8F8">
                 <Familia sobrenome="Pereira">
                     <FamiliaMembro nome="Diva" />
